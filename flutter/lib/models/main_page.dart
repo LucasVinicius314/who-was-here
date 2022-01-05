92712e705be767adc205da00ba77bd6646a341f8
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-01-05 04:39:56 -0300

    ui

--- flutter/lib/models/main_page.dart
+++ flutter/lib/models/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 import 'package:who_was_here/constants/constants.dart';
 import 'package:who_was_here/models/log.dart';
@@ -76,21 +77,45 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(Constants.appName)),
-      body: ListView.builder(
-        itemCount: _logs.length,
-        itemBuilder: (context, index) {
-          final log = _logs.elementAt(index);
+      body: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          ListView.builder(
+            itemCount: _logs.length,
+            padding: const EdgeInsets.only(bottom: 128, top: 4),
+            itemBuilder: (context, index) {
+              final log = _logs.elementAt(index);
 
-          return Card(
-            child: ListTile(
-              title: Text(log.tag),
-              subtitle: Text(
-                DateFormat().format(log.getCreatedAt),
-                style: Theme.of(context).textTheme.caption,
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                child: ListTile(
+                  title: Text(log.tag),
+                  subtitle: Text(
+                    DateFormat().format(log.getCreatedAt),
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  leading:
+                      log.getCreatedAt.hour >= 18 || log.getCreatedAt.hour < 5
+                          ? const Icon(MdiIcons.moonWaxingCrescent)
+                          : const Icon(MdiIcons.whiteBalanceSunny),
+                ),
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Material(
+              borderRadius: const BorderRadius.all(Radius.circular(32)),
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  '${DateFormat('yMMMd').format(DateTime.now())} - ${_logs.length} item${_logs.length == 1 ? '' : 's'}',
+                ),
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
